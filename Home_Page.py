970b5a6520f71b6015eb493c6c58961c10fc8677
Ben Skalka <benjaminskalka@gmail.com>
2024-12-24 23:08:52 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team 75, Web Development - Section B")
+st.subheader("Ben Skalka, Eyan Zupko")
+st.subheader("Introducing Mario and Luigi's Awesome NBA Draft Bot!")
+st.write("The Mario Bros know Ball")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Home Page**: Home Page
+#       2. **Draft Bot**: Ask the Draft Bot!
+#       3. **Draft History**: Sort through NBA draft history
+#       4. **Luigi Portfolio**: Find out about one of our creators!
+#       5. **Mario Portfolio**: Find out about one of our creators!
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. Home Page
+2. Draft Bot
+3. Draft History
+4. Luigi Portfolio
+5. Mario Portfolio
+
+""")
+
---
+++ info.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "It's a me, Mario! I live in the Mushroom Kingdom with my younger brother, Luigi. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/mario-mario-46539733a/"
+my_github_url = "https://github.com/benskall"
+my_email_address = "https://gmail.com"
+
+
+education_data ={
+    'Degree': 'Associate of Science in Plumbing',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["ENGL 1101", "CS 1301", "MATH 1551", "CHEM 1310"], 
+    "names":["English Comp 1", "Intro to Computing", "Differential Calculus", "Intro to Chemistry"], 
+    "semester_taken":["2nd", "3rd", "2nd", "2nd"],
+    "skills":["How to write letters to Peach!", "Coding in Python 🐍", "Relationships between time, velocity, accelaration", "Inner workings of a combustion engine"],
+    }
+experience_data = {
+    "Professional Plumber": (["- Worked on designing a family business",
+                                                                          "- Made Luigi Proud"],"Images/plumb.jpg"),
+    "Race Car Driver for Mario Kart":(["- Sped ahead of everyone after getting a mushroom",
+                                                           "- Stole Toad's head to get a speed boost and win the race"],"Images/kart.jpg"),
+    "Smash Fighter":(["- Fought against enemies of all sorts from differents worlds"],"Images/smash.jpg")
+
+}
+
+projects_data = {
+    "Saving Princess Peach": "Saved the princess from the capture of Donkey Kong",
+    "Coding Draft History Tool": "Coded the app that takes user inputs and outputs a draft history based on an API."
+}
+
+gaming_data = {
+    "Jumping": 90,
+    "Racing": 70,
+    "Running": 45,
+}
+
+#CHANGE BELOW (OPTIONAL)
+gaming_icons = {
+    "Jumping": "⬆️",
+    "Racing": "🏎️",
+    "Running": "🏃",
+}
+spoken_icons = {"Italian": "🇮🇹",
+    "English": "🇬🇧",
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Italian": "Fluent",
+}
+leadership_data = {
+    "Savior of Mushroom Kingdom": (["- Defended the Mushroom Kingdom from Bowser"],"Images/kingdom.jpg"),
+
+}
+activity_data={
+    "Mario and Luigi Brother Service": ["- Cleared bananas on Rainbow Road", 
+            "- Jumped on Goombas"]
+}
